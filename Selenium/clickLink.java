2b8226b9d0f270692ee2ff830fe94c23a9664409
rohitsada <72384700+rohitsada@users.noreply.github.com>
2024-04-24 12:03:43 +0530

    Add files via upload

---
+++ Selenium/clickLink.java
@@ -0,0 +1,23 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class clickLink {
+    public static void main(String[] args) {
+        // Setup the Firefox driver(GeckoDriver)
+        WebDriverManager.firefoxdriver().setup();
+
+        // Create a new instance of the Firefox driver
+        WebDriver driver = new FirefoxDriver();
+        System.out.println(driver.getTitle());
+        driver.get("https://v1.training-support.net");
+        WebElement checkInput=driver.findElement(By.id("about-link"));
+        checkInput.click();
+        System.out.println(driver.getTitle());
+    }
+}
